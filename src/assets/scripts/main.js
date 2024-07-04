1e0383faa374d4ba16ea55c233fe34676e649714
Syed Muhammad Mujtaba Naqvi <smmujtabanaqvi315@gmail.com>
2024-07-04 06:41:27 -0700

    commit

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -51,7 +51,7 @@ $(document).ready(function () {
   
   openCloseHomeSidebar();
 
-  openClosePortfolioSidebar ();
+  openClosePortfolioSidebar();
 
   showcaseIsotopes();
 
@@ -78,13 +78,14 @@ function openClosePortfolioSidebar () {
     $('.portfolio-sidebar').toggleClass('open');
   });
 
-  $(document).load($(window).bind("resize", checkPosition));
+  $(document).on('load', $(window).bind("resize", checkPosition));
   function checkPosition()
   {
       if ($(window).width() > 600){
           $('.portfolio-sidebar').removeClass('open');
       }
   }
+  
 }
 
 // showcase isotopes
@@ -102,6 +103,14 @@ function showcaseIsotopes() {
   $('.portfolio-showcase .filter-button-group').on('click', 'button', function(){
     var filterValue = $(this).attr('data-filter');
     $grid.isotope({ filter: filterValue });
+
+    // Changing number (It has _ items)
+    var numItem = $('.portfolio-showcase ' + filterValue).length;
+    $('.portfolio-showcase .num-of-items .num').html(numItem);
+    var category_name = $(this).html().toLowerCase();
+    var first_letter = category_name[0].toUpperCase();
+    var title_name = category_name.replace(category_name[0], first_letter);
+    $('.portfolio-showcase .num-of-items .name').html(title_name);
   });
 }
 
